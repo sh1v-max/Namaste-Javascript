0d0ee83a4c3856b2b8bf719cd077cb48411b5901
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-19 20:46:03 +0530

    function

--- Codes/02-Functions-in-js.js
+++ Codes/02-Functions-in-js.js
@@ -1,6 +1,7 @@
 var x = 1
 a()
 b()
+sum(5,10)
 
 function a() {
   var x = 10
@@ -10,4 +11,8 @@ function a() {
 function b() {
   var x = 100
   console.log(x)
+}
+
+function sum(a,b) {
+  console.log(a+b)
 }
