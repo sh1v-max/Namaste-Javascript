0a10b56bbdf0582dd2b65902bdb1d66d60c61705
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-20 19:13:18 +0530

    window and this

---
+++ Codes/03_Windon-This-in-js.js
@@ -0,0 +1,8 @@
+// something is created by javascript engine without even
+// writing a single line of code in the File
+// and that is called global object
+// here, we're talking about windows and this keyword
+
+//~ window is a global object which is created along with the global execution context
+//? before any program is run, a global object is created and a global execution context is created
+// and this global object, in case of browsers, called the window object
