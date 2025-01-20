bed188aacff11583db26c002d4de71f610eb3e85
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-20 19:43:40 +0530

    undefined and defined cases

--- Codes/04_Undefined-and-not-defined-in-js.js
+++ Codes/04_Undefined-and-not-defined-in-js.js
@@ -1,3 +1,19 @@
 // undefined is like a placeholder that is placed in the memory when a variable is declared but not assigned a value.
 // not defined is when a variable is not declared at all
 
+console.log(a); // undefined
+var a = 10;
+console.log(a); // 10
+console.log(b); // ReferenceError: b is not defined
+
+// checking defined and undefined cases
+console.log(a)
+var a
+console.log(a)
+a = 5
+if(a === undefined){
+  console.log('a is undefined')
+}
+else{
+  console.log('a is not defined')
+}
