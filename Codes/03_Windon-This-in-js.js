2daec1ce35500f61617cac2650c66c4cfefd1780
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-20 19:23:00 +0530

    global and local variables

--- Codes/03_Windon-This-in-js.js
+++ Codes/03_Windon-This-in-js.js
@@ -6,3 +6,10 @@
 //~ window is a global object which is created along with the global execution context
 //? before any program is run, a global object is created and a global execution context is created
 // and this global object, in case of browsers, called the window object
+//& everything you see in the top level is global scope
+
+
+var a = 10; // global variable
+function b(){
+  var x = 10 // local variable
+}
