1f5ade0efdb18d6946fe8646b5b2ddac226475e8
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-21 19:21:12 +0530

    scope chain done

--- Codes/05_The-scope-chain-in-js.js
+++ Codes/05_The-scope-chain-in-js.js
@@ -1,13 +1,22 @@
 // what is scope?
 //^ Scope is the current context of the code, which determines the accessibility of variables to JavaScript. The two types of scope are global and local.
 
+// what is lexical environment or scope?
+//^ lexical environment is the local memory along with the lexical environment of its parent
+
+
 function a(){
   var b = 10
   c()
   function c(){
-    console.log(b)
+    console.log(b) 
   }
 }
 
 a()
-console.log(b)
+console.log(b) //Uncaught reference undefined error b 
+
+// here lexical environment of c will be it's scope plus lexical environment of its parent
+//? lexical environment
+//? scope
+//? scope chain
