3182d992cc9c2f0c78f8a47db3c6420c30869c98
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-21 16:02:02 +0530

    scope

--- Codes/05_The-scope-chain-in-js.js
+++ Codes/05_The-scope-chain-in-js.js
@@ -1,3 +1,13 @@
 // what is scope?
 //^ Scope is the current context of the code, which determines the accessibility of variables to JavaScript. The two types of scope are global and local.
-//^ or where you can access the specific variable of function in out code is the scope
+
+function a(){
+  var b = 10
+  c()
+  function c(){
+    console.log(b)
+  }
+}
+
+a()
+console.log(b)
