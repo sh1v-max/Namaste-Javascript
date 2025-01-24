177d023257ede5b53d19d36ca2e32c251ba16d17
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-24 17:19:18 +0530

    closures with set timeout

--- Codes/08_Closures.js
+++ Codes/08_Closures.js
@@ -10,7 +10,7 @@ function x(){
   }
   y()
 }
-x()
+// x()
 
 // closure when return a function within one function
 function x() {
@@ -21,9 +21,10 @@ function x() {
   // y();
   return y;//In javascript you can return the functions
 }
-var z = x();
-console.log(z);//It will return what is present in the function y along with closure formed.
-z(); //It will still return the value of a.
+// var z = x;
+// console.log(z);//It will return the function y along with closure formed.
+// console.log(z);//It will return what is present in the function y along with closure formed.
+//z(); //It will still return the value of a.
 
 //? when a function is returned from another function they will still have access to the outer function scope or their lexical scope
 
@@ -33,10 +34,22 @@ function b(){
   function c(){
     var b = 10
     function d(){
-      console.log(a,b)
+      console.log(a,b) 
     }
     d()
   }
   c()
 }
-b()
+// b()
+
+// with setTimeout
+function x(){
+  var i = 1
+  setTimeout(function(){
+    console.log(i)
+  }, 1000)
+}
+x()
+// above settimeout will return 1 even without calling the function x() because of closure
+// cuz this is how javascript even loop works abd the asynchronous nature of setTimeout
+//? it forms closure too
