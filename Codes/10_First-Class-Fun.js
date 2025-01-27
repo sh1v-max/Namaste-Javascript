82362852642196dbd36a89e674c43c3ccf5dd379
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-27 20:08:51 +0530

    function expression

--- Codes/10_First-Class-Fun.js
+++ Codes/10_First-Class-Fun.js
@@ -1,5 +1,55 @@
+//* function statement aka function declaration
+function a(){
+  console.log('I am function statement');
+}
+
+//* function expression
+// a function that is stored in a variable
+var b = function (){
+  console.log('I am function expression');
+}
+
+//& difference between function statement and expression
+// the major difference between these two lies in hoisting
+
+// a() // I am function statement
+// c() // TypeError: c is not a function
+function a(){
+  console.log('I am function statement');
+}
+
+var c = function (){
+  console.log('I am function expression');
+}
+
+// during memory creating phase, a is created in memory and function assigned to a.
+// but b is created like a variable (b:undefined, until code reaches to b)
+// so it doesn't support hoisting 
+
 //* what is anonymous function?
 // a function without a name
 
+//? function (){
+
+// }
+
+// this is going to throw syntax error- function statement requires function name
+// anonymous functions are used at the place of value
+// use anonymous function to assign it to some variable, and not as a function statement
+
+//* named function expression
+// same as function expression but with a name
+var d = function xyz(){
+  console.log('I am named function expression');
+}
+d()
+// xyz() // ReferenceError: xyz is not defined
+// xyz is not available outside the function, created in global scope
+
+
 //* what is first class function? 
-// a function that can be treated as a variable
+// a function that can be treated like any other values
+// can be assigned to a variable
+// can be passed as an argument
+// can be returned from another function
+
