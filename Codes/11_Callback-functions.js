cd7bbc7bc8032a76454b5713492e4a71f7450e90
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-28 20:16:54 +0530

    callback

--- Codes/11_Callback-functions.js
+++ Codes/11_Callback-functions.js
@@ -18,3 +18,11 @@ setTimeout(function () {
 
 //* what is blocking the main thread
 // when we have a long running task, it blocks the main thread, ie the call stack
+// we should never block out main thread
+// we should always use async functions to prevent blocking the main thread
+
+//Event handler and listeners in javascript
+document.getElementById("clickMe")
+    .addEventListener("click", function xyz() {
+        console.log("button clicked");
+    })
