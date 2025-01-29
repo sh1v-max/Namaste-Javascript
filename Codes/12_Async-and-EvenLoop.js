5677da6d59d292d212fe0b5ff767c8e30877d4b3
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-29 18:15:53 +0530

    call stack and callback queue

--- Codes/12_Async-and-EvenLoop.js
+++ Codes/12_Async-and-EvenLoop.js
@@ -2,6 +2,8 @@
 // but in case of async function, the function execution context is created first and then the global execution context is created
 // and in case of setTimeout, the callback function is pushed to the callback queue and then the global execution context is created
 //? and it's controlled by event loop
+//? what event lope does is, it checks the call stack and if it's empty then it pushes the callback function to the call stack
+//! every code execute in the call stack via web APIs
 
 //Event loops
 //Browser functionality 
