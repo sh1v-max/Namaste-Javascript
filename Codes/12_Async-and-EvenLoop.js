ac2585278a102ea3090614ba5f29b0d1656df9ed
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-29 18:21:07 +0530

    push

--- Codes/12_Async-and-EvenLoop.js
+++ Codes/12_Async-and-EvenLoop.js
@@ -1,6 +1,7 @@
 //* usually, global execution context is created first and then the function execution context is created
 // but in case of async function, the function execution context is created first and then the global execution context is created
 // and in case of setTimeout, the callback function is pushed to the callback queue and then the global execution context is created
+// settimeout with 0ms will still goes to the callback queue and then to the call stack
 //? and it's controlled by event loop
 //? what event lope does is, it checks the call stack and if it's empty then it pushes the callback function to the call stack
 //! every code execute in the call stack via web APIs
@@ -17,3 +18,11 @@ setTimeout(function cb() {
     console.log("Callback")
 }, 5000);
 console.log("End")
+
+//Example
+console.log("Start")
+document.getElementById("button")
+    .addEventListener("click", function cb() {  //addeventlistener is the another power given by rhe browser to the engine
+        console.log("CallBack");
+    });
+console.log("End");
