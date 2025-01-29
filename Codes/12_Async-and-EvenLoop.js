7c6cfeddb73415b3e3f4fcc7d3fbe1c39b66732f
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-29 18:00:11 +0530

    event loop and how does it work

--- Codes/12_Async-and-EvenLoop.js
+++ Codes/12_Async-and-EvenLoop.js
@@ -1,3 +1,8 @@
+//* usually, global execution context is created first and then the function execution context is created
+// but in case of async function, the function execution context is created first and then the global execution context is created
+// and in case of setTimeout, the callback function is pushed to the callback queue and then the global execution context is created
+//? and it's controlled by event loop
+
 //Event loops
 //Browser functionality 
 //DOM APIs
