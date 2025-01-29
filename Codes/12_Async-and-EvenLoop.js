601c2e92985da6ca3bc2f9e1ea16e03136fe9b01
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-29 18:22:25 +0530

    event loop with fetch

--- Codes/12_Async-and-EvenLoop.js
+++ Codes/12_Async-and-EvenLoop.js
@@ -21,8 +21,21 @@ console.log("End")
 
 //Example
 console.log("Start")
-document.getElementById("button")
+document.getElementById("clickMe")
     .addEventListener("click", function cb() {  //addeventlistener is the another power given by rhe browser to the engine
         console.log("CallBack");
     });
 console.log("End");
+
+//Example using fetch
+//Example
+console.log("Start")
+setTimeout(function cbT() {
+    console.log(" cb setTimeout")
+}, 5000);
+fetch("https://api.netflix.com")
+    .then(function cbF() {
+        console.log("cb Netflix")
+    })
+
+console.log("End")
