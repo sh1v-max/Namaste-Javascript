c7989548d8a83372e072e2ff0f9866d8d5427cf9
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-30 15:30:43 +0530

    interpreter and compiler

--- Codes/13_JavaScript-engine.js
+++ Codes/13_JavaScript-engine.js
@@ -1,3 +1,4 @@
+//* every browser has a JavaScript engine or the JRE
 //JS run literally everywhere from smart watch to robot to browser
 //* because of JavaScript Runtime Environment (JRE)
 //JRE is a part of the browser that can execute JavaScript code
@@ -9,3 +10,9 @@
 // 5. Event Loop
 // ECMAScript is the standard for JavaScript
 // JavaScript engines: V8(chrome), SpiderMonkey(firefox), Chakra(edge), JavaScriptCore
+
+// In all languages, code is compiled either with interpreter or with compiler. 
+// JS used to have only interpreter in old times, but now has both to compile JS code.
+
+// Interpreter : Takes code and executes line by line. Has no idea what will happen in next line. Very fast.
+// Compiler : Code is compiled and an optimized version of same code is formed, and then executed. More efficient
