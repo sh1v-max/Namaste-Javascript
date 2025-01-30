1ea2c61cd1e3ffd405142900bee04d4a4f3f4c1f
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-30 15:33:06 +0530

    code execution

--- Codes/13_JavaScript-engine.js
+++ Codes/13_JavaScript-engine.js
@@ -16,3 +16,10 @@
 
 // Interpreter : Takes code and executes line by line. Has no idea what will happen in next line. Very fast.
 // Compiler : Code is compiled and an optimized version of same code is formed, and then executed. More efficient
+
+//? Code inside JSE passes through 3 steps : Parsing, Compilation and Execution
+// Parsing : Code is converted into Abstract Syntax Tree(AST)
+// Compilation : AST is converted into Bytecode
+// Execution : Bytecode is executed by the JRE
+
+// v8 of Google has Interpreter called as Ignition, a compiler called Turbo Fan and garbage collector called Orinoco
