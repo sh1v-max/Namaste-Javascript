e97f03dcd970ce3ecf01453b56e0ddd828b18e4c
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-30 12:52:06 +0530

    javascript engine

---
+++ Codes/13_JavaScript-engine.js
@@ -0,0 +1,9 @@
+//JS run literally everywhere from smart watch to robot to browser
+//* because of JavaScript Runtime Environment (JRE)
+//JRE is a part of the browser that can execute JavaScript code
+// JRE consists of:
+// 1. Call Stack
+// 2. Web APIs
+// 3. Microtask Queue
+// 4. Callback Queue
+// 5. Event Loop
