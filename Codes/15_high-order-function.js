3071928e6f3eac5f2960e6e9a61775417ca93424
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-31 19:50:01 +0530

    polyfill for map

--- Codes/15_high-order-function.js
+++ Codes/15_high-order-function.js
@@ -9,7 +9,7 @@ function x(){
 function y(x){
   x()
 }
-y(x)
+// y(x)
 // y is a high order function
 // x is a callback function
 
@@ -25,7 +25,7 @@ const calculateArea = function(radius){
   return output
 }
 
-console.log(calculateArea(radius))
+// console.log(calculateArea(radius))
 
 const calculateCircumference = function(radius){
   const output = []
@@ -35,7 +35,7 @@ const calculateCircumference = function(radius){
   return output
 }
 
-console.log(calculateCircumference(radius))
+// console.log(calculateCircumference(radius))
 
 const calculateDia = function(radius){
   const output = []
@@ -45,12 +45,12 @@ const calculateDia = function(radius){
   return output
 }
 
-console.log(calculateDia(radius))
+// console.log(calculateDia(radius))
 
 // as we can see the above code is repeating itself
 // we can make it better by using high order function
 
-
+//=========================================================
 //* By functional programming using generic function
 const area = function (radius) {
   return Math.PI * radius * radius;
@@ -61,16 +61,47 @@ const circumference = function (radius) {
 const diameter = function (radius) {
   return 2 * radius;
 }
-
+// separate the logic from the function
 // using this, we will directly pass logic to the function
 // as a callback function
 // and that will be a high order function
 
 // and now, to calculate area, circumference and diameter
 
-const calculateArea0 = function (radius, logic) {
+const calculate = function (radius, logic) {
   const output = []
   for(let i = 0; i < radius.length; i++){
     output.push(logic(radius[i]))
   }
+  return output
 }
+
+// console.log(calculate(radius, area))
+// console.log(calculate(radius, circumference))
+// console.log(calculate(radius, diameter))
+
+// HOF require less code and more readable
+// it is more maintainable
+// is reusable
+// we are not repeating a lot of code
+// Map function // although it is same as above function 
+
+
+//=========================================================
+//* polyfill for map function
+Array.prototype.calculate = function (logic) {
+  const output = [];
+  for (let i = 0; i < this.length; i++) {
+      output.push(logic(this[i]))
+  }
+  return output;
+}
+
+console.log(radius.calculate(area));
+console.log(radius.calculate(circumference));
+console.log(radius.calculate(diameter));
+
+// with Map function
+console.log(radius.map(area));
+console.log(radius.map(circumference));
+console.log(radius.map(diameter));
