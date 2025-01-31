5f104ca5322fbe6dc7449de4539cf7f901438436
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-31 18:02:56 +0530

    high order function

--- Codes/14_Trust-issues-with-setTimeout.js
+++ Codes/14_Trust-issues-with-setTimeout.js
@@ -1,3 +1,4 @@
+//& also called concurrency model of JavaScript
 // setTimeout with 5000ms delay
 // it does not always run after 5000ms
 //? trust issues with setTimeout
@@ -8,3 +9,24 @@ setTimeout(function a() {
     console.log("Callback")
 }, 5000);
 console.log("End")
+console.log("End")
+console.log("End")
+console.log("End")
+
+// code demonstration for setTimeout delay
+// it will block your main thread for 10 sec, so even settimeout is set to 0/5000 ms, it will run after 10 sec
+
+let startDate =  new Date().getTime()
+let endDate = startDate
+while (endDate < startDate + 5000) {
+  //as while loop is synchronous, it runs sequentially and blocks the rest of the code execution until it completes
+    endDate = new Date().getTime()
+    // this code will keep call stack busy for 10 sec
+    // and wont allow setTimeout to run
+}
+
+console.log("while expires")
+// setTimeout, in the callback queue, will run after 10 sec
+
+//! first rule of JavaScript: Do not block the main thread
+// javaScrip is synchronous single threaded language
