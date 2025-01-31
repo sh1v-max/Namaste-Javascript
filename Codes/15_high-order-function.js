bf44d804bef1376a9e26bdb85d1f0e69ec471a87
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-31 18:10:54 +0530

    common method

--- Codes/15_high-order-function.js
+++ Codes/15_high-order-function.js
@@ -10,3 +10,22 @@ const calculateArea = function(radius){
 
 console.log(calculateArea(radius))
 
+const calculateCircumference = function(radius){
+  const output = []
+  for(let i = 0; i < radius.length; i++){
+    output.push(2 * Math.PI * radius[i])
+  }
+  return output
+}
+
+console.log(calculateCircumference(radius))
+
+const calculateDia = function(radius){
+  const output = []
+  for(let i = 0; i < radius.length; i++){
+    output.push(2 * radius[i])
+  }
+  return output
+}
+
+console.log(calculateDia(radius))
