92093643491bb4f916cd1aba4089e95883cedb13
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-01-31 19:18:26 +0530

    hof

--- Codes/15_high-order-function.js
+++ Codes/15_high-order-function.js
@@ -1,3 +1,20 @@
+//* what is high order function
+// high order function are regular function that take another functions as 
+// arguments or return functions as their results
+
+// example
+function x(){
+  console.log('hi')
+}
+function y(x){
+  x()
+}
+y(x)
+// y is a high order function
+// x is a callback function
+
+//=========================================================
+// example
 const radius = [3, 1, 2, 4];
 
 const calculateArea = function(radius){
@@ -29,3 +46,31 @@ const calculateDia = function(radius){
 }
 
 console.log(calculateDia(radius))
+
+// as we can see the above code is repeating itself
+// we can make it better by using high order function
+
+
+//* By functional programming using generic function
+const area = function (radius) {
+  return Math.PI * radius * radius;
+}
+const circumference = function (radius) {
+  return 2 * Math.PI * radius;
+}
+const diameter = function (radius) {
+  return 2 * radius;
+}
+
+// using this, we will directly pass logic to the function
+// as a callback function
+// and that will be a high order function
+
+// and now, to calculate area, circumference and diameter
+
+const calculateArea0 = function (radius, logic) {
+  const output = []
+  for(let i = 0; i < radius.length; i++){
+    output.push(logic(radius[i]))
+  }
+}
