6931294095fae6a810cc495a34f6d855d786b26d
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-01 18:41:07 +0530

    map filter and reduce done

--- Codes/16_Map-filter-reduce.js
+++ Codes/16_Map-filter-reduce.js
@@ -37,6 +37,7 @@ const doubleMap = arr.map(function (x) {
 const doubleArrow = arr.map((x) => x * 2)
 // console.log(doubleArrow);
 
+//===============================================================
 //* filter
 // filter is a method that creates a new array with all elements that pass the test implemented by the provided fun
 // returns a new array with the elements that pass the test
@@ -67,6 +68,7 @@ const isOddFilter = arr.filter(function (x) {
 const isOddArrow = arr.filter((x) => x % 2)
 // console.log(isOddArrow);
 
+//===============================================================
 //* reduce
 // reduce is a method that executes a reducer function on each element of the array, resulting in a single output value
 // use reduce when you want to come up with single value as a result
@@ -77,3 +79,82 @@ const isOddArrow = arr.filter((x) => x % 2)
 // currentValue is the current element being processed
 // currentIndex is the index of the current element being processed
 // array is the array reduce was called upon
+
+//? sum of all elements
+// without reduce
+function sumOfArray(x) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+      sum = sum + arr[i];
+  }
+  return sum;
+}
+console.log(sumOfArray(arr))
+
+// with reduce
+const sum = arr.reduce(function (acc, curr) { 
+  acc = acc + curr; // acc acts as a sum
+  return acc; // curr is the current element, arr[i]
+}, 0)
+console.log(sum);
+
+//? max of all elements
+// without reduce
+function maxOfArray(x) {
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+      if (max < arr[i]) {
+          max = arr[i];
+      }
+  }
+  return max;
+}
+console.log(maxOfArray(arr));
+
+// with reduce
+const max = arr.reduce(function (acc, curr) {
+  if (acc < curr) {
+      acc = curr;
+  }
+  return acc;
+}, 0)
+console.log(max)
+
+//===============================================================
+// real life example
+
+const users = [
+  { firstName: "Ed", lastName: "Sheeran", age: 35 },
+  { firstName: "Elon", lastName: "Musk", age: 45 },
+  { firstName: "Singh", lastName: "Wazir", age: 23 },
+  { firstName: "Lana", lastName: "DelRey", age: 25 }
+]
+
+//List of full names, using map
+const fullname = users.map((x) => x.firstName + " " + x.lastName)
+console.log(fullname);
+
+// no of users with particular age, using reduce
+const userNo = users.reduce(function (acc, curr) {
+  if (acc[curr.age]) {
+      acc[curr.age] = ++acc[curr.age];
+  } else {
+      acc[curr.age] = 1;
+  }
+  return acc;
+}, {})
+console.log(userNo);
+
+//first name of user whose age < 30, using filter and map
+const nameAge = users.filter((x) => x.age < 30).map((x) => x.firstName)
+
+console.log(nameAge)
+
+//first name of user whose age < 30 using reduce
+const nameAge30 = users.reduce(function (acc, curr) {
+  if (curr.age < 30) {
+      acc.push(curr.firstName)
+  }
+  return acc
+}, [])
+console.log(nameAge30)
