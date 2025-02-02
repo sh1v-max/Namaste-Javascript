623e09ed39e7c7c10e3854ea79caf1660479d3cc
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-02 17:33:50 +0530

    named solution for callback

--- Codes/17_Callback-hell.js
+++ Codes/17_Callback-hell.js
@@ -4,10 +4,46 @@
 //* callback hell
 // callback hell is a situation where you have multiple nested callbacks which make the code hard to read and debug
 
-console.log("wazir");
+// Example of callback hell
+setTimeout(() => {
+    console.log('1st callback')
+    setTimeout(() => {
+        console.log('2nd callback')
+        setTimeout(() => {
+            console.log('3rd callback')
+            setTimeout(() => {
+                console.log('4th callback')
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
+// this is a simple example of callback hell, it can get more complex with more nested callbacks
+// also know as pyramid of doom
+// hard to read and debug
+//? solution to callback hell
 
-setTimeout(function () {
-    console.log("wazir 2");
-}, 5000);
+//& named functions
+function first() {
+  console.log('1st callback');
+  setTimeout(second, 1000);
+}
 
-console.log("singh")
+function second() {
+  console.log('2nd callback');
+  setTimeout(third, 1000);
+}
+
+function third() {
+  console.log('3rd callback');
+  setTimeout(fourth, 1000);
+}
+
+function fourth() {
+  console.log('4th callback');
+}
+
+setTimeout(first, 1000);
+
+
+// example with e commerce website, its cart
+const cart = ["pants", "shoes", "watch"]
