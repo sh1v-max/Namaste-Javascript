e108e69934b74d6b7bfe1d3b44d8019745f8bb6e
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-03 16:35:20 +0530

    promise uses

--- Codes/18_promises.js
+++ Codes/18_promises.js
@@ -1,5 +1,9 @@
 //* promise
-// a promise is an object that may produce a single value some time in the future
 // it represents the eventual completion (or failure) of an asynchronous operation
+// is an object that may produce a single value some time in the future
 // it allows you to handle asynchronous code in a cleaner way
-// avoid callback hell
+
+//? uses
+// avoiding callback hell (make async code easier to read)
+// handling async operations (api calls, setTimeout, file reading)
+// better error handling (can use .catch())
