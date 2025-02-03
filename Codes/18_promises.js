00af83e549e2c74b4e529317ea92e48b1c8a50d8
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-03 20:24:10 +0530

    promise with async and await

--- Codes/18_promises.js
+++ Codes/18_promises.js
@@ -12,7 +12,7 @@ const promiseOne = new Promise(function(resolve, reject){
   //Do an async task
   // DB calls, cryptography, network
   setTimeout(function(){
-      console.log('Async task is compelete');
+      console.log('Async task is complete');
       resolve()
   }, 1000)
 })
@@ -35,7 +35,7 @@ new Promise(function(resolve, reject){
 // example 3
 const promiseThree = new Promise(function(resolve, reject){
   setTimeout(function(){
-      resolve({username: "Chai", email: "chai@example.com"})
+      resolve({username: "wazir", email: "hello@example.com"})
   }, 1000)
 })
 
@@ -48,7 +48,7 @@ const promiseFour = new Promise(function(resolve, reject){
   setTimeout(function(){
       let error = true
       if (!error) {
-          resolve({username: "hitesh", password: "123"})
+          resolve({username: "wazir", password: "123"})
       } else {
           reject('ERROR: Something went wrong')
       }
@@ -66,7 +66,7 @@ promiseFour
 }).finally(() => console.log("The promise is either resolved or rejected"))
 
 
-
+// example 5
 const promiseFive = new Promise(function(resolve, reject){
   setTimeout(function(){
       let error = true
@@ -77,3 +77,40 @@ const promiseFive = new Promise(function(resolve, reject){
       }
   }, 1000)
 });
+
+async function consumePromiseFive(){
+  try {
+      const response = await promiseFive
+      console.log(response);
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+consumePromiseFive()
+
+
+// example 6, with async await
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+// withe fe
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
