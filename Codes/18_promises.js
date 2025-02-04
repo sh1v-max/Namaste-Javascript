0f82ea7b0a5b08dadfb7297ff41f76198187c68e
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-04 13:50:08 +0530

    promises done

--- Codes/18_promises.js
+++ Codes/18_promises.js
@@ -12,148 +12,172 @@
 // handling async operations (api calls, setTimeout, file reading)
 // better error handling (can use .catch())
 
+//=========================================================
 //? promise is a constructor function, so we need to use new keyword to create a promise object
 const p = new Promise(function(resolve, reject){
   // resolve and reject are functions
   // resolve is called when the promise is resolved
   // reject is called when the promise is rejected
-  resolve('data')
+  resolve('data received')
   reject('error')
 })
 //? a promise generally have 3 states
 // pending: initial state, neither fulfilled nor rejected
 // fulfilled: meaning that the operation completed successfully
 // rejected: meaning that the operation failed
+// and promise offer us 3 methods to handle these states
+// then(), catch(), and finally()
+p.then((data)=>{
+  // then() is called when a promise is resolved
+  console.log('resolved')
+  console.log(data) // data received
+})
+p.catch((err)=>{
+  // catch() is called when a promise is rejected
+  console.log('rejected')
+  console.log(err) // error
+})
+p.finally(()=>{
+  // finally() is called when a promise is resolved or rejected
+  console.log('finally')
+})
+// a promise can only be resolved or rejected once
 
-// const promiseOne = new Promise(function(resolve, reject){
-//   //Do an async task
-//   // DB calls, cryptography, network
-//   setTimeout(function(){
-//       console.log('Async task is complete');
-//       resolve()
-//   }, 1000)
-// })
+//=========================================================
+//? other examples
+const promiseOne = new Promise(function(resolve, reject){
+  //Do an async task
+  // DB calls, cryptography, network
+  setTimeout(function(){
+      console.log('Async task is complete');
+      resolve()
+  }, 1000)
+})
 
-// promiseOne.then(function(){
-//   console.log("Promise consumed");
-// })
-
-// // example 2
-// new Promise(function(resolve, reject){
-//   setTimeout(function(){
-//       console.log("Async task 2");
-//       resolve()
-//   }, 1000)
-
-// }).then(function(){
-//   console.log("Async 2 resolved");
-// })
-
-// // example 3
-// const promiseThree = new Promise(function(resolve, reject){
-//   setTimeout(function(){
-//       resolve({username: "wazir", email: "hello@example.com"})
-//   }, 1000)
-// })
-
-// promiseThree.then(function(user){
-//   console.log(user);
-// })
-
-// // example 4
-// const promiseFour = new Promise(function(resolve, reject){
-//   setTimeout(function(){
-//       let error = true
-//       if (!error) {
-//           resolve({username: "wazir", password: "123"})
-//       } else {
-//           reject('ERROR: Something went wrong')
-//       }
-//   }, 1000)
-// })
-
-// promiseFour
-// .then((user) => {
-//   console.log(user);
-//   return user.username
-// }).then((username) => {
-//   console.log(username);
-// }).catch(function(error){
-//   console.log(error);
-// }).finally(() => console.log("The promise is either resolved or rejected"))
+promiseOne.then(function(){
+  console.log("Promise consumed");
+})
 
 
-// // example 5
-// const promiseFive = new Promise(function(resolve, reject){
-//   setTimeout(function(){
-//       let error = true
-//       if (!error) {
-//           resolve({username: "javascript", password: "123"})
-//       } else {
-//           reject('ERROR: JS went wrong')
-//       }
-//   }, 1000)
-// });
+// example 2
+new Promise(function(resolve, reject){
+  setTimeout(function(){
+      console.log("Async task 2");
+      resolve()
+  }, 1000)
 
-// async function consumePromiseFive(){
-//   try {
-//       const response = await promiseFive
-//       console.log(response);
-//   } catch (error) {
-//       console.log(error);
-//   }
+}).then(function(){
+  console.log("Async 2 resolved");
+})
+
+// example 3
+const promiseThree = new Promise(function(resolve, reject){
+  setTimeout(function(){
+      resolve({username: "wazir", email: "hello@example.com"})
+  }, 1000)
+})
+
+promiseThree.then(function(user){
+  console.log(user);
+})
+
+
+// example 4
+const promiseFour = new Promise(function(resolve, reject){
+  setTimeout(function(){
+      let error = true
+      if (!error) {
+          resolve({username: "wazir", password: "123"})
+      } else {
+          reject('ERROR: Something went wrong')
+      }
+  }, 1000)
+})
+
+promiseFour
+.then((user) => {
+  console.log(user);
+  return user.username
+}).then((username) => {
+  console.log(username);
+}).catch(function(error){
+  console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"))
+
+
+// example 5
+const promiseFive = new Promise(function(resolve, reject){
+  setTimeout(function(){
+      let error = true
+      if (!error) {
+          resolve({username: "javascript", password: "123"})
+      } else {
+          reject('ERROR: JS went wrong')
+      }
+  }, 1000)
+});
+
+async function consumePromiseFive(){
+  try {
+      const response = await promiseFive
+      console.log(response);
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+consumePromiseFive()
+
+
+// example 6, with async await
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
 // }
 
-// consumePromiseFive()
+//getAllUsers()
 
 
-// // example 6, with async await
+// withe fetch
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
 
-// // async function getAllUsers(){
-// //     try {
-// //         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//=========================================================
+// other example
+// without promise
+const cart2 = ["Shoes", "Watches", "Flags"]
 
-// //         const data = await response.json()
-// //         console.log(data);
-// //     } catch (error) {
-// //         console.log("E: ", error);
-// //     }
-// // }
+const orderId2 = createOrder(cart2);
+proceedToPayment(orderId2);
 
-// //getAllUsers()
+createOrder(cart2, function (orderId2) {
+    proceedToPayment(orderId2, function (paymentInf) {
+        showOrderSummary(paymentInf, function () {
+            updateWalletBalance()
+        })
+    })
+});
 
-// // withe fetch
-// fetch('https://api.github.com/users/hiteshchoudhary')
-// .then((response) => {
-//     return response.json()
-// })
-// .then((data) => {
-//     console.log(data);
-// })
-// .catch((error) => console.log(error))
-
-// // other example
-// // without promise
-// const cart2 = ["Shoes", "Watches", "Flags"]
-
-// const orderId2 = createOrder(cart2);
-// proceedToPayment(orderId2);
-
-// createOrder(cart2, function (orderId2) {
-//     proceedToPayment(orderId2, function (paymentInf) {
-//         showOrderSummary(paymentInf, function () {
-//             updateWalletBalance()
-//         })
-//     })
-// });
-
-// // same with promise
-// createOrder(cart2).then(function (orderId2) {
-//   return proceedToPayment(orderId2);
-// })
-//   .then(function (paymentInf) {
-//       return showOrderSummary(paymentInf)
-//   })
-//   .then(function () {
-//       return updateWalletBalance();
-//   })
+// same with promise
+createOrder(cart2).then(function (orderId2) {
+  return proceedToPayment(orderId2);
+})
+  .then(function (paymentInf) {
+      return showOrderSummary(paymentInf)
+  })
+  .then(function () {
+      return updateWalletBalance();
+  })
