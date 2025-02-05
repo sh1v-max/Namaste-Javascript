9187642cd0e66f730d583c3e10732dbcd5be5baa
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-05 20:29:26 +0530

    promise and more

--- Codes/20_more-promise.js
+++ Codes/20_more-promise.js
@@ -15,3 +15,34 @@ createOrder(cart)//return orderId
     .catch(function (err) {
         console.log(err.message);
     })//Handling error and displaying normally in console 
+
+
+    //Creating a promise
+//Producer side code
+function createOrder(cart) {
+  const pr = new Promise(function (resolve, reject) {
+      //validateCart
+      if (!validateCart(cart)) {
+          const err = new Error("Cart is not valid")
+          reject(err);
+      }
+      //CreateOrder
+      const orderId = "12345";
+      if (orderId) {
+          setTimeout(function () {
+              resolve(orderId);
+          }, 2000)
+      }
+  });
+  return pr;
+}
+function procedToPayment() {
+  return new Promise(function (resolve, reject) {
+      resolve("Payment Successful")
+  })
+}
+function validateCart(cart) {
+  // return true;
+   return false;
+// It will throw an error.//Card is not valid
+}
