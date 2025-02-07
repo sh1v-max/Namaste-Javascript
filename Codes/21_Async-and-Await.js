5d63553bcbd9efcc6288d851459346b3713eb2e0
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-07 18:13:06 +0530

    promise with async

--- Codes/21_Async-and-Await.js
+++ Codes/21_Async-and-Await.js
@@ -13,12 +13,29 @@ async function getDate() {
 }
 
 const dataPromise = getDate()
-console.log(dataPromise) // Promise { 'wazir' }
+// console.log(dataPromise) // Promise { 'wazir' }
 // how to get the data from promise
-dataPromise.then(res => console.log(res)) // wazir
+// dataPromise.then(res => console.log(res)) // wazir
 
 
 //* what is await?
+//? async and await, together used to handle promises in a more readable and synchronous way
+// handling promises without async/await
+const p1 = new Promise((resolve, reject) => {
+  resolve("promise resolved")
+})
+
+function getDate1(){
+  p1.then(res => console.log(res))
+}
+getDate1() // promise resolved
+// we were using .then to get the data from promise
+// now, with async/await
+async function handlePromise(){
+  const data = await p1
+  console.log(data)
+}
+handlePromise() // promise resolved
 
 //* how async and await work behind the scene?
 
