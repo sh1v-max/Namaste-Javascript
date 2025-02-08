ff6b1ba390061971e0164814f03c2b0f58d667bc
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-02-08 16:21:03 +0530

    this

--- Codes/22_This.js
+++ Codes/22_This.js
@@ -1,1 +1,22 @@
-console.log('this is this')
+// "use strict";
+//* this keyword
+// this keyword refers to the object it belongs to
+// It has different values depending on where it is used:
+
+//===============================================================
+//? in global space
+// in global space, this refers to global object (window in browser, global in node)
+console.log(this); // window
+
+//===============================================================
+//? inside function
+// based on non-strict mode(default), this will be global object
+// based on strict mode, this will be undefined
+function x() {
+  // the below value depends on strict/non-strict mode
+  console.log(this);
+  // in strict mode - undefined
+  // in non-strict mode - refers to global window object
+}
+x();
+// window.x(); // window
